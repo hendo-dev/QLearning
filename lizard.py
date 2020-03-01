09644532aeac16dea9151a2d47efbde6945163d8
hendo-dev <56521723+hendo-dev@users.noreply.github.com>
2020-02-29 22:32:37 -0500

    lizard fixed 1

--- lizard.py
+++ lizard.py
@@ -1,95 +1,105 @@
-import numpy as np
-def my_print(Q):
-  # hard-coded hack for this problem only
-  rows = len(Q); cols = len(Q[0])
-  print("       0      1      2      3      4      5\
-      6      7      8     ")
-  for i in range(rows):
-    print("%d " % i, end="")
-    if i < 10: print(" ", end="")
-    for j in range(cols): print(" %6.2f" % Q[i,j], end="")
-    print("")
-  print("")
-def path(start, destination, Q):
-      print("-----Solution found-------")
-      i = 1
-      while start != destination:
-            if i >= 10:
-                  break
-            intermediate = np.argmax(Q[start])
-            print(f"{start} -> {intermediate}")
-            start = intermediate
-            i+=1
-      print('-------End---------------')
-      
-def reachable_from(start, valid, number_of_states):
-      candidates = []
-      for state in range(number_of_states):
-            if valid[start][state] == 1:
-                  candidates.append(state)
-      return candidates
+import numpy as np 
 
-def random_movement(start, valid, number_of_states):
-      states = reachable_from(start, valid, number_of_states)
-      return states[np.random.randint(0, len(states))]
+"""
+      Prints Q matrix
+"""
+def m_print(Q):
+      rows = len(Q)
+      cols = len(Q[0])
+      print("       0      1      2      3      4      5\
+      6      7      8")
+      for row in range(rows):
+            print(f"{row} ", end="")
+            if row < 10:
+                  print(" ", end=" ")
+            for col in range(cols):
+                  print(" %6.2f" % Q[row, col], end="")
+            print("")
+      print("")
 
-def learn(valid, reward, discount_factor, learning_rate, destination, number_of_states, episodes):
-      for episode in range(episodes):
+"""
+      Finds all possible states from source
+"""
+def next_states_from(source, feasibility, number_of_states):
+      return [state for state in range(number_of_states) if feasibility[source, state] == 1]
+
+"""
+      Picks a feasible next state from source
+"""
+def next_feasible(source, feasibility, number_of_states):
+      all = next_states_from(source, feasibility, number_of_states)
+      return all[np.random.randint(0, len(all))]
+
+"""
+      Find best state from source at any given time
+"""
+def best(source, feasibility, number_of_states, Q):
+      best = float('-inf')
+      around = next_states_from(source, feasibility, number_of_states)
+      for someone in around:
+            q = Q[source, someone]
+            if q > best:
+                  best = q
+      return best 
+
+"""
+      Computes Q matrix
+"""
+def train(feasibility, reward, Q, gamma, lrn_rate, goal, number_of_states, times):
+      for time in range(times):
+            # Select a random source
             current_state = np.random.randint(0, number_of_states)
-            # print(f"Current state: {current_state}")
-            while current_state != destination:
-                  other = random_movement(current_state, valid, number_of_states)
-                  future_look_up = reachable_from(other, valid, number_of_states)
-                  # Finding best future movement
-                  best = -9999.99
-                  for someone in range(len(future_look_up)):
-                        action = future_look_up[someone]
-                        q_value = Q[other][action]
-                        best = q_value if q_value > best else best
-                  Q[current_state][other] = ((1 - learning_rate) * Q[current_state] \
-                        [other]) + (learning_rate * (reward[current_state][other] + \
-                        (discount_factor * best)))
-                  current_state = other
+            # Updates Q-values while destination is not reached
+            while current_state != goal:
+                  # Select a random next state
+                  next = next_feasible(current_state, feasibility, number_of_states)
+                  # Compute reward
+                  best_choice = best(next, feasibility, number_of_states, Q)
+                  # Update Q
+                  Q[current_state][next] = ((1-lrn_rate) * Q[current_state][next] + (lrn_rate * (reward[current_state][next] + (gamma * best_choice))))
+                  # Moving towards next
+                  current_state = next
 
+"""
+      Prints path from source to goal
+"""
+
+def path(source, goal, Q):
+      while source != goal:
+            next = np.argmax(Q[source])
+            print(f"{source} --> {next}")
+            source = next
 
 if __name__ == '__main__':
-
       np.random.seed(1)
-      # Represent valid moves on the grid
-      valid = np.zeros(shape=[9,9], dtype= np.int)
-      valid[0][1] = 1; valid[0][3] = 1
-      valid[1][0] = 1; valid[1][2] = 1; valid[1][4] = 1
-      valid[2][1] = 1; valid[2][5] = 1
-      valid[3][0] = 1; valid[3][4] = 1; valid[3][6] = 1
-      valid[4][1] = 1; valid[4][3] = 1; valid[4][5] = 1; valid[4][7] = 1
-      valid[5][4] = 1; valid[5][2] = 1; valid[5][8] = 1
-      valid[6][3] = 1; valid[6][7] = 1
-      valid[7][6] = 1; valid[7][4] = 1; valid[7][8] = 1
-      
-      # Represents reward for any combination of states
-      reward = np.zeros(shape=[9,9], dtype=np.float32)
-      for i in range(9):
-            for j in range(9):
-                  reward[i][j] = -0.1
-      # reward[3][0] = 3
-      # reward[1][0] = 3
-      # reward[3][4] = -0.001
-      # reward[1][4] = -0.001
-      # reward[5][4] = -0.001
-      # reward[7][4] = -0.001
-      reward[7][8] = 10.0
+      # Setting arena
+      SIZE = 9
+      feasibility = np.zeros(shape=[SIZE,SIZE], dtype=np.int)
+      feasibility[0][1] = 1; feasibility[0][3] = 1
+      feasibility[3][0] = 1; feasibility[3][4] = 1; feasibility[3][6] = 1
+      feasibility[6][3] = 1; feasibility[6][7] = 1
+      feasibility[1][0] = 1; feasibility[1][4] = 1; feasibility[1][2] = 1
+      feasibility[4][1] = 1; feasibility[4][3] = 1; feasibility[4][5] = 1; feasibility[4][7] = 1
+      feasibility[7][4] = 1; feasibility[7][6] = 1; feasibility[7][8] = 1
+      feasibility[2][1] = 1; feasibility[2][5] = 1
+      feasibility[5][4] = 1; feasibility[5][2] = 1; feasibility[5][8] = 1
+      feasibility[8][8] = 1
+
+      reward = np.zeros(shape=[SIZE,SIZE], dtype=np.float32)
+      for row in range(SIZE):
+            for col in range(SIZE):
+                  reward[row][col] = -0.1
       reward[5][8] = 10.0
-      reward[8][8] = 10.0
+      reward[7][8] = 10.0
+      
+      Q = np.zeros(shape=[SIZE,SIZE], dtype=np.float32)
+      source = 6
+      goal = 8
+      lrn_rate = 0.5
+      gamma = 0.5
+      times = 1000
+      train(feasibility, reward, Q, gamma, lrn_rate, goal, SIZE, times)
 
-      # Represents Q matrix
-      Q = np.zeros(shape=[9,9], dtype=np.float32)
-      start = 6; destination = 8
-      number_of_states = 9
-      discount_factor = 0.5
-      learning_rate = 0.5
-      episodes = 1500
-      learn(valid, reward, discount_factor, learning_rate, destination, number_of_states, episodes)
-      path(start, destination, Q)
-      my_print(Q)
-
-
+      print("------------Q--------------------")
+      m_print(Q)
+      path(source, goal, Q)
